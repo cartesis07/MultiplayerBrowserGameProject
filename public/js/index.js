9ff1da24227e7f8904eae5ca13a923f75c441c72
Hugo Danet <hugo.danet@telecom-paris.fr>
2020-12-26 22:20:06 +0100

    Check if room exists, change username

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,17 @@
 var socket = io.connect();
 
 function CreateRoom(){
-    socket.emit('create', 'test');
+    socket.emit('create', document.getElementById("input-room").value);
 }
 
 function JoinRoom(){
-    socket.emit('join', 'test');
-}
-
-function LeaveRoom(){
-    socket.emit('leave', 'test');
+    socket.emit('join', document.getElementById("input-room").value);
 }
 
 function ShowRooms(){
     socket.emit('ping');
+}
+
+function SendUsername(){
+    socket.emit('send-username', document.getElementById("input-username").value)
 }
--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 
+//list of connected user
+let users = {};
+
+//list of active rooms
+let rooms = {}
+
 //call our express function
 let app = express();
 
@@ -24,24 +30,42 @@ server.listen(port, ()=> {
 });
 
 io.sockets.on('connection', function(socket) {
+
     console.log("A user just connected.")
+    socket.on('send-username', function(username) {
+      console.log("pseudonym changed")
+      users[socket.id] = username;
+    });
+
     socket.on('create', function(room) {
-      console.log("Room created")
       socket.join(room);
+      console.log("Room created with ID : ", room, " by user : ", users[socket.id])
       console.log(socket.rooms);
     });
+
     socket.on('join', function(room){
-      socket.join(room);
-      console.log("Room joined")
-      console.log(socket.rooms);
+
+      //check if this room exists
+      if(io.sockets.adapter.rooms.has(room)){
+        socket.join(room);
+        console.log("Room : ", room, " joined by user : ", users[socket.id])
+        console.log(socket.rooms);
+      }
+      else{
+        console.log("Sorry, but this room does not exist.")
+      }
     })
+
     socket.on('ping', () => {
         console.log(io.sockets.adapter.rooms);
-      });
+    });
+
     socket.on('disconnecting', () => {
         //Do something here
-      });
+    });
+
     socket.on('disconnect', () => {
         console.log('A user has disconnected.');
+        delete users[socket.id];
     })
 });
